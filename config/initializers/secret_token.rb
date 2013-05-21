1640ee5f388bfecacfc2898ecf069ef2fad6627c
Alexander Simonov <alex@simonov.me>
2013-05-22 00:41:28 +0300

    Rails4 new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DevblInUa::Application.config.secret_key_base = 'af3a485b11fb3117de7a0b2c44094e493ee176e3552c59ba46f998712302a83b5ace22b81ae545e96194af0b82b95b36cfc069b286b1134b742869d8c43ac1de'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DevblInUa::Application.config.session_store :cookie_store, key: '_devbl_in_ua_session'
